42e4c0932cda7b84aa46e968d7395549fa8fdb06
Ronald Ray <fritz.ray@eduworks.com>
2018-04-24 11:41:12 -0700

    Updated libraries, polyfilled XMLHttpRequest instead of JQuery, expose all CASS compatible objects. Deployed as 0.3.3

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 var fs = require('fs');
 
-require("node-jquery-xhr");
+var XMLHttpRequest = require('xhr2');
+require("text-encoding");
 var forge = require("node-forge");
 var FormData = require('form-data');
 var antlr4 = require('antlr4/index');
 var pemJwk = require('pem-jwk');
 
-var window = null;
+var window = {crypto:null};
+var Worker = null;
 var document = {};
 var view = {};
 var localStorage = {};
@@ -30,34 +32,50 @@ function load(lib){
 	}
 }
 
+var esprima = require('esprima');
+var results = [];
 eval(load.call(this,"lib/random.js")+"");
 eval(load.call(this,"lib/blobHelper.js")+"");
 eval(load.call(this,"lib/base64toArrayBuffer.js")+"");
 eval(load.call(this,"lib/stjs.js")+"");
 eval(load.call(this,"lib/ec.base.js")+"");
+results.push(esprima.parse(load.call(this,"lib/ec.base.js")+""));
 eval(load.call(this,"lib/ec.crypto.js")+"");
+results.push(esprima.parse(load.call(this,"lib/ec.crypto.js")+""));
 eval(load.call(this,"lib/org.json-ld.js")+"");
+results.push(esprima.parse(load.call(this,"lib/org.json-ld.js")+""));
 eval(load.call(this,"lib/org.cassproject.schema.general.js")+"");
+results.push(esprima.parse(load.call(this,"lib/org.cassproject.schema.general.js")+""));
 eval(load.call(this,"lib/org.schema.js")+"");
+results.push(esprima.parse(load.call(this,"lib/org.schema.js")+""));
 eval(load.call(this,"lib/org.w3.skos.js")+"");
+results.push(esprima.parse(load.call(this,"lib/org.w3.skos.js")+""));
 eval(load.call(this,"lib/org.cassproject.schema.ebac.js")+"");
+results.push(esprima.parse(load.call(this,"lib/org.cassproject.schema.ebac.js")+""));
 eval(load.call(this,"lib/org.cassproject.schema.cass.js")+"");
+results.push(esprima.parse(load.call(this,"lib/org.cassproject.schema.cass.js")+""));
 eval(load.call(this,"lib/ebac.identity.js")+"");
+results.push(esprima.parse(load.call(this,"lib/ebac.identity.js")+""));
 eval(load.call(this,"lib/ebac.repository.js")+"");
+results.push(esprima.parse(load.call(this,"lib/ebac.repository.js")+""));
 eval(load.call(this,"lib/cass.competency.js")+"");
+results.push(esprima.parse(load.call(this,"lib/cass.competency.js")+""));
 eval(load.call(this,"lib/RollupListener.js")+"");
 eval(load.call(this,"lib/RollupLexer.js")+"");
 eval(load.call(this,"lib/RollupParser.js")+"");
 eval(load.call(this,"lib/cass.rollup.js")+"");
+results.push(esprima.parse(load.call(this,"lib/cass.rollup.js")+""));
 
-global.EcRemote = EcRemote;
-global.EcRepository = EcRepository;
-global.EcRemoteIdentityManager = EcRemoteIdentityManager;
-global.EcIdentityManager = EcIdentityManager;
-global.EcFramework = EcFramework;
-global.EcCompetency = EcCompetency;
-global.EcAlignment = EcAlignment;
-global.EcLevel = EcLevel;
-global.EcPk = EcPk;
-global.EcPpk = EcPpk;
-global.EcAssertion = EcAssertion;
+var decls = "";
+
+for (var j = 0;j < results.length;j++)
+for (var i = 0;i < results[j].body.length;i++)
+	if (results[j].body[i].declarations != null)
+decls += "\nglobal."+results[j].body[i].declarations[0].id.name+" = " + results[j].body[i].declarations[0].id.name;
+
+eval(decls);
+
+global.forge = forge;
+global.FormData = FormData;
+global.antlr4 = antlr4;
+global.pemJwk = pemJwk;

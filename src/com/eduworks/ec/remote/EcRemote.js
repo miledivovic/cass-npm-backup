0ee73c92c3edef03e762a184ef86b75e7b8f4f70
Mile Divovic <md2950@nyu.edu>
2022-07-05 17:03:45 -0700

    fix logging error

--- src/com/eduworks/ec/remote/EcRemote.js
+++ src/com/eduworks/ec/remote/EcRemote.js
@@ -250,7 +250,7 @@ module.exports = class EcRemote {
 			})
 			.catch((err) => {
 				if (err != null) {
-					global.auditLogger.report(global.auditLogger.LogCategory.NETWORK, global.auditLogger.Severity.ERROR, "EcRemotePostInner", err && err.response && err.response.request.socket ? err.response.request.socket.remoteAddress : '', url, postHeaders, err);
+					global.auditLogger.report(global.auditLogger.LogCategory.NETWORK, global.auditLogger.Severity.ERROR, "EcRemotePostInner", err && err.response && err.response.request && err.response.request.socket ? err.response.request.socket.remoteAddress : '', url, postHeaders, err);
 					if (err.response != null) {
 						if (err.response.data != null)
 							throw err.response.data;
@@ -301,7 +301,7 @@ module.exports = class EcRemote {
 				return response.data;
 			})
 			.catch((err) => {
-				global.auditLogger.report(global.auditLogger.LogCategory.NETWORK, global.auditLogger.Severity.ERROR, "EcRemoteGetExpectString", err && err.response && err.response.request.socket ? err.response.request.socket.remoteAddress : '', url, err);
+				global.auditLogger.report(global.auditLogger.LogCategory.NETWORK, global.auditLogger.Severity.ERROR, "EcRemoteGetExpectString", err && err.response && err.response.request && err.response.request.socket ? err.response.request.socket.remoteAddress : '', url, err);
 				if (err) {
 					if (err.response) {
 						if (err.response.data)
@@ -348,7 +348,7 @@ module.exports = class EcRemote {
 				return response.data;
 			})
 			.catch((err) => {
-				global.auditLogger.report(global.auditLogger.LogCategory.NETWORK, global.auditLogger.Severity.ERROR, "EcRemoteDelete", err && err.response && err.response.request.socket ? err.response.request.socket.remoteAddress : '', url, signatureSheet, err);
+				global.auditLogger.report(global.auditLogger.LogCategory.NETWORK, global.auditLogger.Severity.ERROR, "EcRemoteDelete", err && err.response && err.response.request && err.response.request.socket ? err.response.request.socket.remoteAddress : '', url, signatureSheet, err);
 				if (err) {
 					if (err.response) {
 						if (err.response.data)

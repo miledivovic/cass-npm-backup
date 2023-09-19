31d16fa9ac5903669a8e544dc703b7efcdc4a8fd
Fritz Ray <fritz.ray@eduworks.com>
2023-09-19 13:59:18 -0700

    1.5.36

--- src/com/eduworks/ec/crypto/EcCrypto.js
+++ src/com/eduworks/ec/crypto/EcCrypto.js
@@ -11,7 +11,7 @@ module.exports = class EcCrypto {
 	 *  @type boolean
 	 */
 	static caching = false;
-	static testMode = true;
+	static testMode = false;
 	static deprecationNotice = false;
 	static decryptionCache = {};
 	static md5Cache = {};

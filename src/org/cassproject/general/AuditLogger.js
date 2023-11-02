a5b8763c7d968fd72ee9bbc733f5adda5458a5f6
Fritz Ray <fritz.ray@eduworks.com>
2023-11-02 10:49:38 -0700

    Failing to log error messages logs an error message.

--- src/org/cassproject/general/AuditLogger.js
+++ src/org/cassproject/general/AuditLogger.js
@@ -1,10 +1,14 @@
 if (!global.auditLogger) { // Running client-side
     global.auditLogger = {
         report: function(system, severity, message, ...data) {
-            if (severity === 'error') {
-                console.trace(system + ": " + severity + ": " + message + ": " + data);
-            } else {
-                console.log(system + ": " + severity + ": " + message + ": " + data);
+            try {
+                if (severity === 'error') {
+                    console.trace(system + ": " + severity + ": " + message + ": " + data);
+                } else {
+                    console.log(system + ": " + severity + ": " + message + ": " + data);
+                }
+            } catch (ex) {
+                console.trace("Could not log error message.");
             }
         },
         LogCategory: {},

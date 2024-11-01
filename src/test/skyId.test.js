75d49e8e0f185ffc0117a37f10d8e1aeadd8aaa1
Ronald Ray <fritz.ray@eduworks.com>
2024-11-01 16:26:12 -0700

    Updated tests.

--- src/test/skyId.test.js
+++ src/test/skyId.test.js
@@ -28,7 +28,7 @@ describe('SkyID Adapter', function() {
         ident.displayName = name;
         im.addIdentity(ident);
         await rld.commit(null, null, im);
-    }).timeout(2000);
+    }).timeout(6000);
 
     it('change password', async () => {
         let rld = new EcRemoteIdentityManager();
@@ -39,7 +39,7 @@ describe('SkyID Adapter', function() {
         assert.equal(im.ids[0].ppk.toPem(), ident.ppk.toPem());
         rld.changePassword(username, password, newPassword);
         await rld.commit(null, null, im);
-    }).timeout(2000);
+    }).timeout(6000);
 
     it('load user', async () => {
         let rld = new EcRemoteIdentityManager();
@@ -48,5 +48,5 @@ describe('SkyID Adapter', function() {
         rld.startLogin(username, newPassword);
         let im = await rld.fetch();
         assert.equal(im.ids[0].ppk.toPem(), ident.ppk.toPem());
-    }).timeout(2000);
+    }).timeout(6000);
 });

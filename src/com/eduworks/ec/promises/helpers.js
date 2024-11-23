6ad2a90dbacf9f33502dc63e253fb7af10b2bb5d
Fritz Ray <fritz.ray@eduworks.com>
2024-11-22 16:58:33 -0800

    Release 1.5.70

--- src/com/eduworks/ec/promises/helpers.js
+++ src/com/eduworks/ec/promises/helpers.js
@@ -9,6 +9,18 @@ module.exports = {
             p = p.catch(failure);
         return p;
     },
+    cassReturnNullAsPromise: function (success, failure, error) {
+        let p = new Promise((resolve, reject) => {
+            resolve(null);
+        });
+        if (success !== undefined && success != null && failure !== undefined && failure != null)
+            p = p.then(success, failure);
+        else if (success !== undefined && success != null)
+            p = p.then(success);
+        else if (failure !== undefined && failure != null)
+            p = p.catch(failure);
+        return p;
+    },
     cassReturnAsPromise: function (o, success, failure, error) {
         let p = new Promise((resolve, reject) => {
             if (o === undefined || o == null) {
